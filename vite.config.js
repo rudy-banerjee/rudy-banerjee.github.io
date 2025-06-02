019c0cb16b82a2a43c341cb48b639eb9d727ea43
Rudy Banerjee <rudyb2001@gmail.com>
2025-06-01 21:01:54 -0700

    Update path based on NODE_ENV

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rudy-banerjee.github.io/',
+  base: process.env.NODE_ENV === 'production' ? '/rudy-banerjee.github.io/' : '/',
   server: {
     port: 3000,
     open: true
