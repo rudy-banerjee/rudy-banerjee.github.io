b4ffb949e047fc8d3d03683d0442cad34deae4ba
Rudy Banerjee <rudyb2001@gmail.com>
2025-06-01 22:21:03 -0700

    Add blogs index for sorting by date

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogPost from '../components/BlogPost.jsx';
 
 function Blogs() {
-  // List of blog files in order (most recent first)
-  const blogFiles = [
-    'blog-1.md',
-    'blog-2.md'
-  ];
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/blogs/index.json')
+      .then(res => res.json())
+      .then(posts => {
+        // Sort by date descending (newest first)
+        const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+        setBlogPosts(sortedPosts);
+      })
+      .catch(err => console.error('Error loading blog index:', err));
+  }, []);
 
   return (
     <div className="blogs-page">
-      {blogFiles.map((blogFile, index) => (
-        <BlogPost key={index} blogFile={blogFile} />
+      {blogPosts.map((post, index) => (
+        <BlogPost key={index} blogFile={post.file} />
       ))}
     </div>
   );
