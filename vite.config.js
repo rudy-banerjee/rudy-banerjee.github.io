f5d3e37ee10133889452aa331ba010a4779f210e
Rudy Banerjee <rudyb2001@gmail.com>
2025-06-01 21:17:28 -0700

    Revert vite config base url to '/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/rudy-banerjee.github.io/' : '/',
+  base: '/',
   server: {
     port: 3000,
     open: true
